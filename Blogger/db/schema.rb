52840ec60de994384a789eb298f5662f491da500
Jason <jason.epstein89@gmail.com>
2017-03-23 14:35:57 -0400

    added a few gems

--- Blogger/db/schema.rb
+++ Blogger/db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321003700) do
+ActiveRecord::Schema.define(version: 20170322231525) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_filename"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
